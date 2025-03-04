6fc7b5f7fe410ccfe292a4bff679abe2a6c50507
Cort Schaefer <cort@eschaefer.net>
2025-03-04 12:26:50 -0700

    attempt to fix ef test failure that passes locally

--- src/EntityFramework/test/IdentityServer4.EntityFramework.Tests/IntegrationTest.cs
+++ src/EntityFramework/test/IdentityServer4.EntityFramework.Tests/IntegrationTest.cs
@@ -29,14 +29,14 @@ namespace IdentityServer4.EntityFramework.IntegrationTests {
                 TestDatabaseProviders = new TheoryData<DbContextOptions<TDbContext>>
                 {
                     DatabaseProviderBuilder.BuildInMemory<TDbContext>(typeof(TClass).Name),
-                    DatabaseProviderBuilder.BuildSqlite<TDbContext>(typeof(TClass).Name),
-                    DatabaseProviderBuilder.BuildLocalDb<TDbContext>(typeof(TClass).Name)
+                    DatabaseProviderBuilder.BuildSqlite<TDbContext>(typeof(TClass).Name)
+                    //,DatabaseProviderBuilder.BuildLocalDb<TDbContext>(typeof(TClass).Name)
                 };
             } else {
                 TestDatabaseProviders = new TheoryData<DbContextOptions<TDbContext>>
                 {
-                    DatabaseProviderBuilder.BuildInMemory<TDbContext>(typeof(TClass).Name)
-                    //, DatabaseProviderBuilder.BuildSqlite<TDbContext>(typeof(TClass).Name)
+                    DatabaseProviderBuilder.BuildInMemory<TDbContext>(typeof(TClass).Name),
+                    DatabaseProviderBuilder.BuildSqlite<TDbContext>(typeof(TClass).Name)
                 };
                 Console.WriteLine("Skipping DB integration tests on non-Windows");
             }
